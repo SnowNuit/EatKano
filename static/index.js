4dde75b08f6ef0e8df26acc143786ff4ea4fdcd9
SnowNuit <98890757+SnowNuit@users.noreply.github.com>
2022-02-03 15:52:07 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '加油！再來一次？';
+        if (cps <= 8) return '好耶';
+        if (cps <= 10)  return '不錯呦！';
+        if (cps <= 15) return '嗚哇？！';
+        return '啊這OwO';
     }
 
     function toStr(obj) {
